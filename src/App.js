dc869622369275b431b9de95d7ef579345395825
Estelle Comment <estelle.comment@gmail.com>
2019-01-04 15:01:02 +0100

    add child nodes when a node is clicked. Has bug where all parents of the clicked node add a child node, not just the clicked node.

--- src/App.js
+++ src/App.js
@@ -3,36 +3,62 @@ import treeRoot from './tree-root.svg';
 import './App.css';
 
 
-const Branch = function () {
+// props : {id, top, left}
+class Node extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { children: [] };
+    this.id = props.id;
+    this.childIdCounter = 0;
 
-};
+    this.branchSizePx = 500;
+  }
 
-const Node = function ({bottom, top, left, right, children}) {
-  return (
-      <div className="node"
-           style={{
-             bottom: bottom,
-             top: top,
-             left: left,
-             right: right
-           }}>
-        <div className="circle"/>
-        {children}
-      </div>
-  );
+  addChild() {
+    this.setState((oldState) => {
+      let childId = this.id + '-' + this.childIdCounter;
+      this.childIdCounter ++;
+      let topPx = - Math.random() * this.branchSizePx;
+      let leftPx = Math.random() * this.branchSizePx - this.branchSizePx/2;
+      oldState.children.push({id : childId, topPx: topPx, leftPx: leftPx});
+      return oldState;
+    });
+  }
+
+  render() {
+    return (
+        <div className="node"
+             style={{
+               top: this.props.topPx + "px",
+               left: this.props.leftPx + "px"
+             }}
+             onClick={(e) => { // event is picked up by all parents of the clicked circle. Stop propagation?
+               e.preventDefault();
+               console.log('onclick for node ', this.id, 'target:', e.target, 'currentTarget', e.currentTarget);
+               this.addChild();
+             }}
+        >
+          <div className="circle"/>
+          <div>{this.id}</div>
+          {this.state.children.map((child) => {
+            return <Node key={child.id}
+                         id={child.id}
+                         topPx={child.topPx}
+                         leftPx={child.leftPx}
+            />
+          })}
+        </div>
+    );
+  }
 };
 
 class Tree extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
         <div className="background">
           <div className="tree-root">
             <img src={treeRoot} width="300" alt="tree root"></img>
-            <Node>
+            <Node id={0}>
             </Node>
           </div>
         </div>
