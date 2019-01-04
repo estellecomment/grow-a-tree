d6a51d12b9080b85a3177962adb7274a9d732c30
Estelle Comment <estelle.comment@gmail.com>
2019-01-04 14:00:44 +0100

    tree root and root node

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import treeRoot from './tree-root.svg';
 import './App.css';
+
+
+const Branch = function () {
+
+};
+
+const Node = function ({bottom, top, left, right, children}) {
+  return (
+      <div className="node"
+           style={{
+             bottom: bottom,
+             top: top,
+             left: left,
+             right: right
+           }}>
+        <div className="circle"/>
+        {children}
+      </div>
+  );
+};
+
+class Tree extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div className="background">
+          <div className="tree-root">
+            <img src={treeRoot} width="300" alt="tree root"></img>
+            <Node>
+            </Node>
+          </div>
+        </div>
+    );
+  };
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <Tree/>
+        </div>
     );
   }
 }
